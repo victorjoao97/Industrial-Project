ef3d4eaa72cedb373cfb184a8303852f289614e2
Valeria <lera0349314@yandex.ru>
2020-05-21 05:31:31 +0300

    First try to implement "select" with all resources

---
+++ EnergyAndMaterialBalanceModule/Controllers/MainController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using EnergyAndMaterialBalanceModule.Models;
+using EnergyAndMaterialBalanceModule.Data;
+using Microsoft.EntityFrameworkCore;
+using EnergyAndMaterialBalanceModule.Data.Repositories;
+
+namespace EnergyAndMaterialBalanceModule.Controllers
+{
+    public class MainController : Controller
+    {
+        private readonly ILogger<MainController> _logger;
+        private readonly IResourcesRepository _resourceRepository;
+
+
+        public MainController(ILogger<MainController> logger, IResourcesRepository resourceRepository)
+        {
+            _logger = logger;
+            _resourceRepository = resourceRepository;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Resources> resources = await _resourceRepository.GetAllResources();
+            return View(resources);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
