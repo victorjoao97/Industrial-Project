ee8d3b5d3c6afdce274ac7e4de9a81c75e4b5ad6
Valeria <lera0349314@yandex.ru>
2020-05-25 11:43:47 +0300

    Added get method to select specific resource

--- EnergyAndMaterialBalanceModule/Controllers/MainController.cs
+++ EnergyAndMaterialBalanceModule/Controllers/MainController.cs
@@ -9,21 +9,22 @@ using EnergyAndMaterialBalanceModule.Models;
 using EnergyAndMaterialBalanceModule.Data;
 using Microsoft.EntityFrameworkCore;
 using EnergyAndMaterialBalanceModule.Data.Repositories;
+using Microsoft.AspNetCore.Http;
 
 namespace EnergyAndMaterialBalanceModule.Controllers
-{
+{ 
     public class MainController : Controller
     {
         private readonly ILogger<MainController> _logger;
         private readonly IResourcesRepository _resourceRepository;
 
+        const string SessionSelectedResource = "_Resource";
 
         public MainController(ILogger<MainController> logger, IResourcesRepository resourceRepository)
         {
             _logger = logger;
             _resourceRepository = resourceRepository;
         }
-
 
         public async Task<IActionResult> Index()
         {
@@ -32,7 +33,16 @@ namespace EnergyAndMaterialBalanceModule.Controllers
             return View();
         }
 
-       
+       [HttpGet]
+        public async Task<IActionResult> Index(int id)
+        {
+            IEnumerable<Resources> resources = await _resourceRepository.GetAllResources();
+            ViewData["Resources"] = resources;
+            HttpContext.Session.SetInt32(SessionSelectedResource, id);
+            ViewData["SelectedResource"] = HttpContext.Session.GetInt32(SessionSelectedResource);
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
