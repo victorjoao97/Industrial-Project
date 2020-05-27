727e7fba3cd43bdbbfa9b20e64935e790d1d5832
m.sheinberg <m.sheinberg@skbkontur.ru>
2020-05-27 15:50:07 +0300

    BGroupsRepository inited

---
+++ EnergyAndMaterialBalanceModule/Data/Repositories/BGroupsRepository.cs
@@ -0,0 +1,26 @@
+﻿using EnergyAndMaterialBalanceModule.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EnergyAndMaterialBalanceModule.Data.Repositories
+{
+    public class BGroupsRepository : GenericRepository<Bgroups, SEICBalanceContext>, IBGroupsRepository
+    {
+        public BGroupsRepository(SEICBalanceContext context) : base(context)
+        {
+
+        }
+
+        public IEnumerable<Bgroups> GetAllBgroups(int resourceId)
+        {
+            return Context.Bgroups.Where(t => t.ResourceId == resourceId).AsEnumerable();
+        }
+
+        public IEnumerable<Bgroups> GetRootBGroups(int resourceId)
+        {
+            return Context.Bgroups.Where(t => t.ResourceId == resourceId && t.BgroupIdparent == null).AsEnumerable();
+        }
+    }
+}
