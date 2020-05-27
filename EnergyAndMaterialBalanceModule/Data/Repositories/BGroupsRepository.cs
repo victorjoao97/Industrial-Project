2c06ab9bef1064217de27685b15d751a9cc67889
Anjasmoro Adi Nugroho <anjasmoro.adi29@gmail.com>
2020-05-27 23:49:44 +0400

    Add BGroupsRepository

---
+++ EnergyAndMaterialBalanceModule/Data/Repositories/BGroupsRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EnergyAndMaterialBalanceModule.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace EnergyAndMaterialBalanceModule.Data.Repositories
+{
+    public class BGroupsRepository : BaseRepository<Bgroups, SEICBalanceContext>, IBGroupsRepository
+    {
+        public BGroupsRepository(SEICBalanceContext context) : base(context)
+        {
+
+        }
+
+        public async Task<IEnumerable<Bgroups>> GetAllBgroups()
+        {
+            return await GetAll().ToListAsync();
+        }
+
+        public async Task<IEnumerable<Bgroups>> GetRootBgroups(int resourceId)
+        {
+            return await Context.Set<Bgroups>().Where(b => b.ResourceId == resourceId && b.BgroupIdparent == null).Include(f => f.InverseBgroupIdparentNavigation).ToListAsync();
+        }
+
+        public IEnumerable<Bgroups> GetChildren(int parentId)
+        {
+            return Context.Set<Bgroups>().Where(f => (f.BgroupIdparent == parentId)).ToList();
+        }
+
+        public Bgroups GetById(int id)
+        {
+            return Context.Set<Bgroups>().Include(f => f.InverseBgroupIdparentNavigation).Where(b => b.BgroupId == id).First();
+        }
+
+    }
+}
---
+++ EnergyAndMaterialBalanceModule/Data/Repositories/IBGroupsRepository.cs
@@ -0,0 +1,16 @@
+﻿using EnergyAndMaterialBalanceModule.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EnergyAndMaterialBalanceModule.Data.Repositories
+{
+    public interface IBGroupsRepository
+    {
+        Task<IEnumerable<Bgroups>> GetAllBgroups();
+        Task<IEnumerable<Bgroups>> GetRootBgroups(int resourceId);
+        IEnumerable<Bgroups> GetChildren(int parentId);
+        Bgroups GetById(int id);
+    }
+}
