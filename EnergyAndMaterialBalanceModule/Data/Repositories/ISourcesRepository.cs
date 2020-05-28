aa40b42f7cfe2e974693b72857a9595d14090db1
m.sheinberg <m.sheinberg@skbkontur.ru>
2020-05-28 12:29:26 +0300

    Source repository fixed

--- EnergyAndMaterialBalanceModule/Data/Repositories/ISourcesRepository.cs
+++ EnergyAndMaterialBalanceModule/Data/Repositories/ISourcesRepository.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using EnergyAndMaterialBalanceModule.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace EnergyAndMaterialBalanceModule.Data.Repositories
 {
-    interface ISourcesRepository
+    interface ISourcesRepository : IBaseRepository<Sources>
     {
     }
 }
