0e3822f02009d21a0202f5439447eb31be5680f9
m.sheinberg <m.sheinberg@skbkontur.ru>
2020-05-28 11:17:40 +0300

    Sources and PeriodsRepository are created

---
+++ EnergyAndMaterialBalanceModule/Data/Repositories/ISourcesRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EnergyAndMaterialBalanceModule.Data.Repositories
+{
+    interface ISourcesRepository
+    {
+    }
+}
